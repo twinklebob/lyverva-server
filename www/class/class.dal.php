becad5360dde95bdb760e9b8264d8916c7bb4b7b
David Lumm (Tablet) <david.lumm@twinklebob.co.uk>
2014-07-16 23:22:46 +0100

    Added table create scripts, more work on DAL

--- www/class/class.dal.php
+++ www/class/class.dal.php
@@ -1,16 +1,37 @@
 <?php
 class DAL {
 	public $sTableName;
+	public $sIdFieldName;
 	public $iId;
 	public $aClassExludeVars = array();
+	public $oDb;
 	
 	public function __construct($iId = 0) {
+		// TODO: Create DB Connection
+		
+		// If specific item requested, load it
+		if($iId != 0) {
+			$this->load();
+		}
 	}
 	
 	public function load() {
+		$sSql = "SELECT * FROM [" . $this->sTableName . "] WHERE [" . $this->sIdFieldName . "] = ?";
+		$aResults = $this->oDb->queryFirst($sSql, $this->iId);
+		if($aResults) {
+			$this->putClassVars($aResults);
+		} else {
+			throw new ErrorException(get_class($this) . ": No results found.");
+		}
 	}
 	
 	public function save() {
+		$aData = $this->getClassVars();
+		if($this->iId == 0) {
+			// TODO: Insert
+		} else {
+			// TODO: Update
+		}
 	}
 	
 	private function getClassVars() {
@@ -22,6 +43,11 @@ class DAL {
 	}
 	
 	private function putClassVars($aData) {
+		// TODO: Cycle through each property
+		
+		// TODO: Strip type
+		
+		// TODO: Update value if it is in aData
 	}
 }
 ?>
