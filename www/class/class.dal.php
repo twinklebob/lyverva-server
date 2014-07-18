d41ad7a5b09f21d1c6170e4f2daca5d0eb0bd0a8
David Lumm (budoc) <david.lumm@twinklebob.co.uk>
2014-07-19 00:10:39 +0100

    Added DB class Added DB Config Updated DAL shell

--- www/class/class.dal.php
+++ www/class/class.dal.php
@@ -1,21 +1,37 @@
 <?php
-class DAL {
-	public $sTableName;
-	public $sIdFieldName;
+class lyvDAL {
+	protected $aIgnoreSave = array("sTableName", "aIgnoreSave", "aClassExludeVars", "dtCreatedate", "iCreateuser", "dtModifydate", "iModifyuser");
+	protected $sTableName;
+	protected $sIdFieldName;
+	protected $aClassExludeVars = array();
+	
+	protected $oDb;
+	
 	public $iId;
-	public $aClassExludeVars = array();
-	public $oDb;
+	
+	public $dtCreatedate = 0;
+	public $iCreateuser = 0;
+	public $dtModifydate = 0;
+	public $iModifyuser = 0;
 	
 	public function __construct($iId = 0) {
 		// TODO: Create DB Connection
+		$this->oDb = Database::obtain(); 
 		
 		// If specific item requested, load it
 		if($iId != 0) {
+			$this->iId;
 			$this->load();
+		} else {
+			// TODO: Default create/modify values
 		}
 	}
 	
 	public function load() {
+		// First check DB connection
+		if(!isset($this->oDb) || empty($this->oDb) || !$this->oDb->bIsConnected) {
+			throw new ErrorException(get_class($this) . " Load: Unable to connect to database.");
+		}
 		$sSql = "SELECT * FROM [" . $this->sTableName . "] WHERE [" . $this->sIdFieldName . "] = ?";
 		$aResults = $this->oDb->queryFirst($sSql, $this->iId);
 		if($aResults) {
@@ -25,12 +41,15 @@ class DAL {
 		}
 	}
 	
-	public function save() {
+	public function save($iUserId = 0) {
 		$aData = $this->getClassVars();
 		if($this->iId == 0) {
 			// TODO: Insert
 		} else {
 			// TODO: Update
+			// Get modifydate and modifyuser
+			$aData['modifydate'] = date("Y-m-d H:i");
+			$aData['modifyuser'] = $iUserId;
 		}
 	}
 	
---
+++ www/include/config.inc.php
@@ -0,0 +1,11 @@
+//database server
+define('DB_SERVER', "twinklebob.dns-systems.net");
+
+//database login name
+define('DB_USER', "twinklebob_lyv");
+
+//database login password
+define('DB_PASS', "Tw1nkl3b0b");
+
+//database name
+define('DB_DATABASE', "twinklebob_lyv");
